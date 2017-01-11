2cee11512e7f747b761435335daf5c91606d119f
pavannaga <pavannaganna@Pavans-MacBook-Pro.local>
2017-01-10 23:15:44 -0800

    Adding comments

--- recipes/default.rb
+++ recipes/default.rb
@@ -7,6 +7,7 @@ tomcat_service_name = 'tomcat8'
 tomcat_user_group = "tomcat_#{tomcat_service_name}"
 include_recipe 'java'
 
+# Install tomcat 8.0.36 version and configures required user and group
 tomcat_install tomcat_service_name do
   version '8.0.36'
   exclude_docs false
@@ -14,6 +15,7 @@ tomcat_install tomcat_service_name do
   exclude_manager false
 end
 
+# Creates a directory to store the ssl certs and key
 directory '/usr/local/ssl/' do
   owner tomcat_user_group
   group tomcat_user_group
@@ -22,6 +24,9 @@ directory '/usr/local/ssl/' do
   action :create
 end
 
+# Creates server.crt file and server.pem - this file is a self signed certificate.
+# See more at https://www.linux.com/learn/creating-self-signed-ssl-certificates-apache-linux
+# For APR implementation, see https://tomcat.apache.org/tomcat-6.0-doc/apr.html
 cookbook_file '/usr/local/ssl/server.crt' do
   source 'server.crt'
   owner tomcat_user_group
@@ -36,6 +41,8 @@ cookbook_file '/usr/local/ssl/server.pem' do
   mode 00777
 end
 
+# Creates (actually copy) the keystore file for JSSE implementation in tomcat
+# JSSE is currently implemented using this cookbook.
 cookbook_file '/usr/local/ssl/tomcat' do
   source 'tomcat'
   owner tomcat_user_group
@@ -47,6 +54,7 @@ tomcat_service tomcat_service_name do
   action [:enable, :start]
 end
 
+# Creates server.xml file with JSSE configs
 template "/opt/tomcat_#{tomcat_service_name}/conf/server.xml" do
   source 'server.xml.erb'
   owner tomcat_user_group
